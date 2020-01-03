bd62278385dab27841781d4f4518242e290dc89e
Jamie Mitch <7iny.d4nc3r@gmail.com>
2020-01-02 20:03:36 -0500

    updated environments to include todosUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './models';
+
+export const environment: Environment = {
+  production: true,
+  todosUrl: 'prod url goes here'
 };
